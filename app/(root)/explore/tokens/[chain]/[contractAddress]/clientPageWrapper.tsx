1a30f4883af04251772bb50d87ee658a2a21f029
Abbas Ali <abbasali.mehtar@gmail.com>
2024-10-28 16:11:06 +0400

    boom

--- app/(root)/explore/tokens/[chain]/[contractAddress]/clientPageWrapper.tsx
+++ app/(root)/explore/tokens/[chain]/[contractAddress]/clientPageWrapper.tsx
@@ -6,12 +6,12 @@ import SwapWidget from "@/components/SwapWidget";
 import { Button } from "@/components/ui/button";
 import TokenMiniChart from "@/components/charts/TokenMiniChart";
 import {
-  Table,
-  TableHeader,
-  TableRow,
-  TableHead,
-  TableBody,
-  TableCell,
+    Table,
+    TableHeader,
+    TableRow,
+    TableHead,
+    TableBody,
+    TableCell,
 } from "@/components/ui/table";
 import { MOCK_TOKEN_EXPLORE_PAGE_STATS } from "@/constants";
 import { formatNumberWithCommas, cn, formatNumberToKMB } from "@/lib/utils";
@@ -26,60 +26,60 @@ import { Skeleton } from "@/components/ui/skeleton";
 const MOCK_NAME = "X";
 
 const Page = ({ contractAddress, chartData, tokenInfo, chain }: any) => {
-  const { data: tokenPriceData, isLoading: priceLoading } = useFetchTokenPrice(
-    contractAddress,
-    chain
-  );
-    
+    const { data: tokenPriceData, isLoading: priceLoading } = useFetchTokenPrice(
+        contractAddress,
+        chain
+    );
+
     const [currentTable, setCurrentTable] = useState("Transactions");
 
-  const marketCap = useMemo(() => {
-    const lastPrice =
-      chartData[chartData.length - 1]?.price ??
-      (!priceLoading ? tokenPriceData : 0);
-    const marketCap = lastPrice * tokenInfo.getTokenInfo?.circulatingSupply;
-    return marketCap;
-  }, [chartData, tokenPriceData, priceLoading, tokenInfo]);
+    const marketCap = useMemo(() => {
+        const lastPrice =
+            chartData[chartData.length - 1]?.price ??
+            (!priceLoading ? tokenPriceData : 0);
+        const marketCap = lastPrice * tokenInfo.getTokenInfo?.circulatingSupply;
+        return marketCap;
+    }, [chartData, tokenPriceData, priceLoading, tokenInfo]);
 
-  const fdv = useMemo(() => {
-    const lastPrice =
-      chartData[chartData.length - 1]?.price ??
-      (!priceLoading ? tokenPriceData : 0);
-    const totalSupply = tokenInfo.getTokenInfo?.totalSupply;
-    const fdv = lastPrice * totalSupply;
-    return fdv;
-  }, [chartData, tokenPriceData, priceLoading, tokenInfo]);
+    const fdv = useMemo(() => {
+        const lastPrice =
+            chartData[chartData.length - 1]?.price ??
+            (!priceLoading ? tokenPriceData : 0);
+        const totalSupply = tokenInfo.getTokenInfo?.totalSupply;
+        const fdv = lastPrice * totalSupply;
+        return fdv;
+    }, [chartData, tokenPriceData, priceLoading, tokenInfo]);
 
-  const tokenPrice = useMemo(() => {
-    const lastPrice =
-      chartData[chartData.length - 1]?.price ??
-      (!priceLoading ? tokenPriceData : 0);
-    return lastPrice as number;
-  }, [chartData, tokenPriceData, priceLoading]);
+    const tokenPrice = useMemo(() => {
+        const lastPrice =
+            chartData[chartData.length - 1]?.price ??
+            (!priceLoading ? tokenPriceData : 0);
+        return lastPrice as number;
+    }, [chartData, tokenPriceData, priceLoading]);
 
-  return (
-    <div className="my-10">
-      <ExploreBreadcrumb
-        contractAddress={contractAddress}
-        name={(tokenInfo.getTokenInfo?.symbol).toUpperCase() || MOCK_NAME}
-      />
+    return (
+        <div className="my-10">
+            <ExploreBreadcrumb
+                contractAddress={contractAddress}
+                name={(tokenInfo.getTokenInfo?.symbol).toUpperCase() || MOCK_NAME}
+            />
 
-      <div className="flex justify-between items-start gap-4 ">
-        <TokenChart
-          data={chartData}
-          tokenInfo={tokenInfo.getTokenInfo}
-          tokenPrice={tokenPrice}
-          priceLoading={priceLoading}
-        />
-        <div className="mt-10">
-          <div className="flex flex-col gap-4 z-90">
-            <SwapTokenSelectorModalWrapper />
-          </div>
-          <SwapWidget />
-          <Listeners />
-        </div>
-      </div>
-      {/* <div className="flex gap-2 mt-4 bg-[#191919] p-2 rounded-lg w-fit">
+            <div className="flex justify-between items-start gap-4 ">
+                <TokenChart
+                    data={chartData}
+                    tokenInfo={tokenInfo.getTokenInfo}
+                    tokenPrice={tokenPrice}
+                    priceLoading={priceLoading}
+                />
+                <div className="mt-10">
+                    <div className="flex flex-col gap-4 z-90">
+                        <SwapTokenSelectorModalWrapper />
+                    </div>
+                    <SwapWidget />
+                    <Listeners />
+                </div>
+            </div>
+            {/* <div className="flex gap-2 mt-4 bg-[#191919] p-2 rounded-lg w-fit">
                 {TIME_RANGES.map((range) => (
                     <button
                         key={range.label}
@@ -96,85 +96,83 @@ const Page = ({ contractAddress, chartData, tokenInfo, chain }: any) => {
                     </button>
                 ))}
             </div> */}
-      <div className="mt-10 flex flex-col gap-6 ">
-        <h2 className="text-[28px] font-semibold">Stats</h2>
+            <div className="mt-10 flex flex-col gap-6 ">
+                <h2 className="text-[28px] font-semibold">Stats</h2>
 
-        <div className="text-white lining-nums flex justify-between border-b border-white/5 pb-10">
-          <div className="flex flex-col gap-1">
-            <h3 className="text-white/50">TVL</h3>
-            <p className="text-[28px]">$239.8M</p>
-          </div>
-          <div className="flex flex-col gap-1">
-            <h3 className="text-white/50">Market cap</h3>
-            <p className="text-[28px]">
-              {marketCap ? (
-                formatNumberToKMB(marketCap)
-              ) : priceLoading ? (
-                <Skeleton className="h-[40px] w-[100px] rounded-[10px]" />
-              ) : (
-                formatNumberToKMB(marketCap)
-              )}
-            </p>
-          </div>
-          <div className="flex flex-col gap-1">
-            <h3 className="text-white/50">FDV</h3>
-            <p className="text-[28px]">
-              {fdv ? (
-                formatNumberToKMB(fdv)
-              ) : priceLoading ? (
-                <Skeleton className="h-[40px] w-[100px] rounded-[10px]" />
-              ) : (
-                formatNumberToKMB(fdv)
-              )}
-            </p>
-          </div>
-          <div className="flex flex-col gap-1">
-            <h3 className="text-white/50">1 day volume</h3>
-            <p className="text-[28px]">$438.2M</p>
-          </div>
-        </div>
-      </div>
+                <div className="text-white lining-nums flex justify-between border-b border-white/5 pb-10">
+                    <div className="flex flex-col gap-1">
+                        <h3 className="text-white/50">TVL</h3>
+                        <p className="text-[28px]">$239.8M</p>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <h3 className="text-white/50">Market cap</h3>
+                        <p className="text-[28px]">
+                            {marketCap ? (
+                                formatNumberToKMB(marketCap)
+                            ) : priceLoading ? (
+                                <Skeleton className="h-[40px] w-[100px] rounded-[10px]" />
+                            ) : (
+                                formatNumberToKMB(marketCap)
+                            )}
+                        </p>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <h3 className="text-white/50">FDV</h3>
+                        <p className="text-[28px]">
+                            {fdv ? (
+                                formatNumberToKMB(fdv)
+                            ) : priceLoading ? (
+                                <Skeleton className="h-[40px] w-[100px] rounded-[10px]" />
+                            ) : (
+                                formatNumberToKMB(fdv)
+                            )}
+                        </p>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <h3 className="text-white/50">1 day volume</h3>
+                        <p className="text-[28px]">$438.2M</p>
+                    </div>
+                </div>
+            </div>
 
-      <div className="flex gap-0 pb-3 mt-10">
-        <Button
-          className={`transition-all duration-200 text-[28px] ${
-            currentTable === "Transactions"
-              ? "text-white hover:opacity-50"
-              : "text-white/50 hover:text-white"
-          }`}
-          onClick={() => setCurrentTable("Transactions")}
-        >
-          Transactions
-        </Button>
+            <div className="flex gap-0 pb-3 mt-10">
+                <Button
+                    className={`transition-all duration-200 text-[28px] ${currentTable === "Transactions"
+                        ? "text-white hover:opacity-50"
+                        : "text-white/50 hover:text-white"
+                        }`}
+                    onClick={() => setCurrentTable("Transactions")}
+                >
+                    Transactions
+                </Button>
 
-        <Button
-          className={`transition-all duration-200 text-[28px] ${
-            currentTable === "Pools"
-              ? "text-white hover:opacity-50"
-              : "text-white/50 hover:text-white"
-          }`}
-          onClick={() => setCurrentTable("Pools")}
-        >
-          Pools
-        </Button>
-      </div>
+                <Button
+                    className={`transition-all duration-200 text-[28px] ${currentTable === "Pools"
+                        ? "text-white hover:opacity-50"
+                        : "text-white/50 hover:text-white"
+                        }`}
+                    onClick={() => setCurrentTable("Pools")}
+                >
+                    Pools
+                </Button>
+            </div>
 
-      <div className="explore-table-border-container flex justify-center items-center bg-fading-gradient relative">
-        {/*
+            <div className="explore-table-border-container flex justify-center items-center bg-fading-gradient relative">
+                {/*
         FADED GRADIENT BORDER TOP + BOTTOM
         Increase/Decrease the opacity of the gradient to make it more/less visible 👇
         */}
-        <div className="bg-[#191919] explore-table-container m-[1px] pb-6">
-          {currentTable === "Transactions" && (
-            <TransactionTable address={contractAddress} />
-          )}
-          {currentTable === "Pools" && (
-            <TokenPoolTable address={contractAddress} />
-          )}
+                <div className="bg-[#191919] explore-table-container m-[1px] pb-6">
+                    {currentTable === "Transactions" && (
+                        <TransactionTable address={contractAddress} />
+                    )}
+                    {currentTable === "Pools" && (
+                        <TokenPoolTable address={contractAddress} />
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Page;
