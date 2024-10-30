6890e066e32da4747c6aecd79ea6bda0e25e08a6
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2024-10-30 18:43:07 +0500

    optimize: remove swap page

--- app/(root)/swap/page.tsx
+++ app/(root)/swap/page.tsx
@@ -1,7 +1,19 @@
-import SwapPage from "./components/swapPage";
+import SwapWidget from "@/components/SwapWidget";
+import SwapAndRampSwitch from "@/components/SwapAndRampSwitch";
+import SwapTokensInfoSection from "@/components/SwapTokensInfoSection";
 
 const Page = () => {
-  return <SwapPage />;
+  return (
+    <div className="flex flex-col items-center justify-center ">
+      <div className="py-8 w-full px-0 sm:px-6 md:px-10 max-w-[530px] ">
+        <SwapAndRampSwitch />
+
+        <SwapWidget className="max-w-full" />
+
+        <SwapTokensInfoSection />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
